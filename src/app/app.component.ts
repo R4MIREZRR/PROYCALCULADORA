7a969c09ed099383b1fc9b329b2b06e54911b2a7
rodrigor <r4mirez.r@gmail.com>
2024-02-02 10:52:16 -0500

    Se agrego la logica funcia resta, suma, tc.. falta historial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-@Component({
+@Component ({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'PROYCALCULADORA';
+
+
+export class AppComponent implements OnInit{
+  title = 'PROYECTO CALCULADORA';
+  screen= "";
+  a:any;
+  b:any;
+  c:any;
+  d="";
+  e="";
+
+  ngOnInit() {
+
+  }
+
+//   Calculadora
+
+    enterValue(value:string){
+      if((this.b=="+") || (this.b=="-") || (this.b=="*") || (this.b=="/")){
+        this.d = this.d + value;
+        this.screen = this.screen + value;
+        this.c = this.d;
+
+        console.log("op1-> ",this.b,this.c);
+
+      }else{
+        this.screen = this.screen + value;
+        this.a = this.screen;
+        console.log("op2-> ", this.a);
+      }
+    }
+
+    condition(value: string){
+        this.screen = this.screen + value;
+        this.b = value;
+    }
+
+    clear(){
+        this.screen= "";
+        this.a="";
+        this.b="";
+        this.c="";
+        this.d="";
+    }
+
+    result(){
+
+      if(this.b == '+'){
+        this.screen = `${this.screen} = ${(parseInt(this.a) + parseInt(this.c)).toString()}` ;
+        this.screen = (parseInt(this.screen) + parseInt(this.c)).toString();
+        console.log(this.a, this.b, this.c, '=', this.screen);
+      }
+
+        if(this.b == '-'){
+            this.screen = `${this.screen} = ${(parseInt(this.a) - parseInt(this.c)).toString()}` ;
+            this.screen = (parseInt(this.screen) - parseInt(this.c)).toString();
+            console.log(this.a, this.b, this.c, '=', this.screen);
+        }
+
+        if(this.b == '*'){
+            this.screen = `${this.screen} = ${(parseInt(this.a) * parseInt(this.c)).toString()}` ;
+            this.screen = (parseInt(this.screen) * parseInt(this.c)).toString();
+            console.log(this.a, this.b, this.c, '=', this.screen);
+        }
+
+        if(this.b == '/'){
+            this.screen = `${this.screen} = ${(parseInt(this.a) / parseInt(this.c)).toString()}` ;
+            this.screen = (parseInt(this.screen) / parseInt(this.c)).toString();
+            console.log(this.a, this.b, this.c, '=', this.screen);
+        }
+
+      this.clear();
+
+    }
+
+
 }
